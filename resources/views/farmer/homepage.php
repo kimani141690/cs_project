e4740554ae7ae5fa5752d57cf0d8f18362c34502
Sandra <105887486+sandrawg@users.noreply.github.com>
2023-07-14 01:24:45 +0300

    Add files via upload

---
+++ resources/views/farmer/homepage.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <nav>
+        <div class="logo">
+        <img src="d.png" alt="Logo">
+    </div>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Services</a>
+                <ul>
+                    <li><a href="#">Products</a></li>
+                    <li><a href="#">Orders</a></li>
+                    <li><a href="#">Farm Details</a></li>
+                    <li><a href="#">Test</a></li>
+                </ul>
+            </li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Profile</a></li>
+        </ul>
+    </nav>
+    <h1>Welcome to DairyLink</h1>
+    <p>Welcome to DairyLink, your go-to mobile app for accurate<br>dairy production forecasts and direct connections between dairy farmers and consumers.</p>
+    <h2>Services</h2>
+    <div class="container">
+    <a href="products.html" class="box">
+        <div class="color-box"></div>
+        <div class="label">Products</div>
+    </a>
+    <a href="orders.html" class="box">
+        <div class="color-box"></div>
+        <div class="label">Orders</div>
+    </a>
+    <a href="farm-details.html" class="box">
+        <div class="color-box"></div>
+        <div class="label">Farm Details</div>
+    </a>
+    <a href="test.html" class="box">
+        <div class="color-box"></div>
+        <div class="label">Test</div>
+    </a>
+</div>
+</body>
+</html>
